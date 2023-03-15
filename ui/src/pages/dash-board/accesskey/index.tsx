8cef3171602712c2e0d2802caa7bd692c2115de8
baibingjie <18610754492@163.com>
2023-03-15 17:39:02 +0800

    feat:merge code

--- ui/src/pages/dash-board/accesskey/index.tsx
+++ ui/src/pages/dash-board/accesskey/index.tsx
@@ -7,8 +7,9 @@ import {
 } from '@ant-design/pro-components';
 import { Button, message, Popconfirm } from 'antd';
 import React, { useEffect, useRef, useState } from 'react';
-import { history } from 'umi';
+import { history, useModel } from 'umi';
 import AccessKeyForm from '../components/AccessKeyForm';
+import CommonNamespace from '../components/CommonNamespace';
 import styles from './../config/index.less';
 import {
   deleteAccessKey,
@@ -26,12 +27,19 @@ const TableList: React.FC<unknown> = () => {
   const [formType, setFormType] = useState(''); // 弹窗类型，新建、编辑、查看
   const [makeakVisibel, setMakeakVisibel] = useState<boolean>(false);
   const [roleTypeBollean, setRoleTypeBollean] = useState<boolean>(true);
+
+  const namespace = JSON.parse(
+    // @ts-ignore
+    localStorage.getItem('use-local-storage-state-namespace'),
+  );
+  // @ts-ignore
+  const { select } = useModel('useUser');
   const actionRef = useRef<any>();
 
   const handleRemove = async (ak: string) => {
     let res;
     if (roleTypeBollean) {
-      res = await deleteAccessKey({ namespace: 'comos', ak: ak });
+      res = await deleteAccessKey({ namespace: namespace.value, ak: ak });
     } else {
       res = await deleteSystermListAccessKey({ ak: ak });
     }
@@ -106,7 +114,7 @@ const TableList: React.FC<unknown> = () => {
                 {
                   pathname: roleTypeBollean
                     ? `/dash-board/accesskey/detail`
-                    : `/system-config/accesskey/detail`,
+                    : `/system/accesskey/detail`,
                   search: record.ak,
                 },
                 { ak: record.ak },
@@ -159,126 +167,134 @@ const TableList: React.FC<unknown> = () => {
     }
   }, [history.location.pathname]);
 
-  return (
-    <PageContainer
-      header={{
-        title: 'AccessKey管理',
-      }}
-    >
-      <ProTable<API.UserInfo>
-        actionRef={actionRef}
-        rowKey="ak"
-        search={{
-          labelWidth: 60,
-        }}
-        options={false}
-        headerTitle={[
-          <Button
-            key="ketnew"
-            type="primary"
-            onClick={() => {
-              setMakeakVisibel(true);
-              setFormType('新建');
-            }}
-            style={{
-              display: roleTypeBollean ? 'block' : 'none',
-            }}
-          >
-            新建
-          </Button>,
-        ]}
-        request={async (params, sorter, filter) => {
-          let tableData = [];
-          if (roleTypeBollean) {
-            let res = await getAccessKeyList({
-              namespace: 'comos',
-            });
-            if (res?.code == 0 && res?.data?.items.length > 0) {
-              tableData = res.data.items;
-            }
-          } else {
-            let res = await getSystermAccessKeyList({});
-            if (res?.code == 0 && res?.data?.items.length > 0) {
-              tableData = res.data.items;
-            }
-          }
+  useEffect(() => {
+    actionRef?.current.reload();
+  }, [select]);
 
-          return {
-            data: tableData || [],
-            // success,
-          };
-        }}
-        columns={columns}
-        rowClassName={(record, index) => {
-          let className = styles.lightRow;
-          if (index % 2 === 1) className = styles.darkRow;
-          return className;
-        }}
-      />
-      <ModalForm<{
-        alias: string;
-      }>
-        layout="horizontal"
-        open={makeakVisibel}
-        title="新建AccessKey"
-        autoFocusFirstInput
-        modalProps={{
-          destroyOnClose: true,
-          onCancel: () => setMakeakVisibel(false),
-        }}
-        submitTimeout={2000}
-        onFinish={async (values: { alias: string }) => {
-          let res = await postNewAccesskey({
-            namespace: 'comos',
-            alias: values.alias,
-          });
-          if (res?.code == 0) {
-            setFormValues({
-              ak: res.data.ak,
-              password: res.data.password,
-            });
-            setMakeakVisibel(false);
-            handleUpdateModalVisible(true);
-            actionRef?.current.reload();
-          } else {
-            message.error(res?.msg || '操作失败，请稍后再试');
-          }
+  return (
+    <>
+      <CommonNamespace />
+      <PageContainer
+        header={{
+          title: 'AccessKey管理',
         }}
       >
-        <ProFormText
-          width="md"
-          name="alias"
-          label="AccessKey"
-          placeholder="请填写AccessKey"
-          fieldProps={{
-            max: 16,
+        <ProTable<API.UserInfo>
+          actionRef={actionRef}
+          rowKey="ak"
+          search={{
+            labelWidth: 60,
           }}
-          rules={[{ required: true, message: '请填写AccessKey' }]}
-        />
-      </ModalForm>
+          options={false}
+          headerTitle={[
+            <Button
+              key="ketnew"
+              type="primary"
+              onClick={() => {
+                setMakeakVisibel(true);
+                setFormType('新建');
+              }}
+              style={{
+                display: roleTypeBollean ? 'block' : 'none',
+              }}
+            >
+              新建
+            </Button>,
+          ]}
+          request={async (params, sorter, filter) => {
+            let tableData = [];
+            if (roleTypeBollean) {
+              let res = await getAccessKeyList({
+                // @ts-ignore
+                namespace: namespace.value,
+              });
+              if (res?.code == 0 && res?.data?.items.length > 0) {
+                tableData = res.data.items;
+              }
+            } else {
+              let res = await getSystermAccessKeyList({});
+              if (res?.code == 0 && res?.data?.items.length > 0) {
+                tableData = res.data.items;
+              }
+            }
 
-      {/* 更新 */}
-      <AccessKeyForm
-        formType={formType}
-        onSubmit={async (value) => {
-          handleUpdateModalVisible(false);
-        }}
-        onCancel={() => {
-          handleUpdateModalVisible(false);
-        }}
-        updateModalVisible={updateModalVisible}
-        values={formValues}
-        onDownLoad={() => {
-          const result = formValues;
-          const content = JSON.stringify(result);
-          const data = new Blob([content], {
-            type: 'application/json,charset=utf-8;',
-          });
-          downloadFun(data, 'config.json');
-          handleUpdateModalVisible(false);
-        }}
-      />
-    </PageContainer>
+            return {
+              data: tableData || [],
+              // success,
+            };
+          }}
+          columns={columns}
+          rowClassName={(record, index) => {
+            let className = styles.lightRow;
+            if (index % 2 === 1) className = styles.darkRow;
+            return className;
+          }}
+        />
+        <ModalForm<{
+          alias: string;
+        }>
+          layout="horizontal"
+          open={makeakVisibel}
+          title="新建AccessKey"
+          autoFocusFirstInput
+          modalProps={{
+            destroyOnClose: true,
+            onCancel: () => setMakeakVisibel(false),
+          }}
+          submitTimeout={2000}
+          onFinish={async (values: { alias: string }) => {
+            let res = await postNewAccesskey({
+              namespace: namespace.value,
+              alias: values.alias,
+            });
+            if (res?.code == 0) {
+              setFormValues({
+                ak: res.data.ak,
+                password: res.data.password,
+              });
+              setMakeakVisibel(false);
+              handleUpdateModalVisible(true);
+              actionRef?.current.reload();
+            } else {
+              message.error(res?.msg || '操作失败，请稍后再试');
+            }
+          }}
+        >
+          <ProFormText
+            width="md"
+            name="alias"
+            label="AccessKey"
+            placeholder="请填写AccessKey"
+            fieldProps={{
+              max: 16,
+            }}
+            rules={[{ required: true, message: '请填写AccessKey' }]}
+          />
+        </ModalForm>
+
+        {/* 更新 */}
+        <AccessKeyForm
+          formType={formType}
+          onSubmit={async (value) => {
+            handleUpdateModalVisible(false);
+          }}
+          onCancel={() => {
+            handleUpdateModalVisible(false);
+          }}
+          updateModalVisible={updateModalVisible}
+          values={formValues}
+          onDownLoad={() => {
+            const result = formValues;
+            const content = JSON.stringify(result);
+            const data = new Blob([content], {
+              type: 'application/json,charset=utf-8;',
+            });
+            downloadFun(data, 'config.json');
+            handleUpdateModalVisible(false);
+          }}
+        />
+      </PageContainer>
+    </>
   );
 };
 
